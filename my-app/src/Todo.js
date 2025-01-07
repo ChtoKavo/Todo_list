159575773e3400e0ece3de7fef1c50de89b27a40
ChtoKogo <Yra222225522@gmail.com>
2025-01-07 13:07:01 +0400

    изменено

--- my-app/src/Todo.js
+++ my-app/src/Todo.js
@@ -5,7 +5,7 @@ const Todo = ({ todo, onDelete }) => {
   return (
     <div class = "aaa">
       <span>{todo.text}</span>
-      <button class = "deletebt" onClick={() => onDelete(todo.id)}>Удалить</button>
+      <button class = "deletebt" onClick={() => onDelete(todo.id)}>Удалить, да, удалить </button>
     </div>
   );
 };
